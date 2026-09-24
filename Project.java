e555dfa49bdef1c0b9a83657da00c2d3cf33d85c
JoaoVitor.html <ipnhuxgamer@gmail.com>
2026-09-24 16:35:04 -0300

    Add Protect class with database table creation SQL

---
+++ Project.java
@@ -0,0 +1,20 @@
+public final class Protect {
+    
+    private Protect() {
+    }
+
+   
+    public static final String CREATE_CLIENTES_TABLE =
+            "CREATE TABLE clientes ("
+                    + "id_cliente INT AUTO_INCREMENT PRIMARY KEY, "
+                    + "nome VARCHAR(100), "
+                    + "cpf VARCHAR(11), "
+                    + "data_nascimento DATE, "
+                    + "celular VARCHAR(15)"
+                    + ")";
+
+    
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/digitalbank";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "sua_senha_do_banco";
+}
