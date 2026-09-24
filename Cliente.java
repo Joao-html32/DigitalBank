287ec5d9f407d417bbd2853992a9888f542a4b66
JoaoVitor.html <ipnhuxgamer@gmail.com>
2026-09-24 16:33:13 -0300

    Create Cliente class with basic attributes and methods

---
+++ Cliente.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Cliente {
+   
+    private String nome;
+    private String CPF;
+    private LocalDate dataDeNascimento;
+    private String celular;
+
+    public Cliente(String nome, String CPF, LocalDate dataDeNascimento, String celular) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataDeNascimento = dataDeNascimento;
+        this.celular = celular;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public String getCPF() {
+        return this.CPF;
+    }
+    public LocalDate getDataDeNascimento() {
+        return this.dataDeNascimento;
+    }
+
+    // Getter para o Celular
+    public String getCelular() {
+        return this.celular;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+   
+}
